8d6267646dd17d3da2c0a4d266c319757208ee80
Divyansh Aggarwal <divyanshaggarwal2904@gmail.com>
2025-09-19 01:07:33 +0530

    Added database.py

--- app/config.py
+++ app/config.py
@@ -1,1 +1,7 @@
 from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    DATABASE_URL: str = "sqlite:///./lms.db" 
+    DEFAULT_LEAVE_BALANCE: int = 20
+
+settings = Settings()
